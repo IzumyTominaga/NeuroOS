193359245532d4cd1027548b4178939044461c51
CientiFik12 <chidosvlogs@gmail.com>
2026-06-04 05:51:41 -0700

    Add Multiboot entry point

---
+++ boot.asm
@@ -0,0 +1,34 @@
+MBALIGN  equ 1<<0
+MEMINFO  equ 1<<1
+FLAGS    equ MBALIGN | MEMINFO
+MAGIC    equ 0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS)
+
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
+
+section .text
+bits 32
+global _start
+extern kernel_main
+
+_start:
+    mov esp, stack_top
+    ; EAX = magic de multiboot (0x2BADB002)
+    ; EBX = puntero a estructura multiboot_info
+    push ebx        ; multiboot_info*
+    push eax        ; magic
+    call kernel_main
+    cli
+.hang:
+    hlt
+    jmp .hang
